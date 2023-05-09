1772d10b13f52d047484784473541a9ddcee3c99
Moamen Hassaballah <hassaballahmoamen84@gmail.com>
2023-05-10 02:49:16 +0300

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Moamen Hassaballah on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
